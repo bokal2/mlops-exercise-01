f7c199c63c6a4f6942a2c94ef10c3a5ab3e4f838
Erick Okal <bobbydasilva98@gmail.com>
2025-07-19 21:53:04 +0400

    Added health status endpoint

--- src/main.py
+++ src/main.py
@@ -1,6 +1,15 @@
 import math
+from fastapi import FastAPI
+
+
+app = FastAPI()
 
 
 def calculate_hypotenus(a: int, b: int) -> float:
     c = math.sqrt(a**2 + b**2)
     return round(c, 2)
+
+
+@app.get("/")
+def healthy():
+    return {"status": "Running"}
---
+++ tests/e2e/test_api.py
@@ -0,0 +1,11 @@
+from fastapi import status
+from fastapi.testclient import TestClient
+
+from src.main import app
+
+client = TestClient(app)
+
+
+def test_healthy_endpoint():
+    response = client.get("/")
+    assert response.status_code == status.HTTP_200_OK
